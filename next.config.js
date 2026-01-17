cb4677ea566979f5f8943b6a993535dc83fe283e
Marko Vuckovic <markoair@Markos-MacBook-Air.local>
2026-01-17 19:27:34 +0100

    Fix pdfkit font asset issue

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: {
-      bodySizeLimit: '10mb',
-    },
+    serverActions: { bodySizeLimit: '10mb' },
+    serverComponentsExternalPackages: ['pdfkit'],
   },
 }
 
 module.exports = nextConfig
+
